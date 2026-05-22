e77780f22cc5d212ac63fc5b830df455d24512bb
delcioleonardorofino <delcioleonard@gmail.com>
2026-05-22 20:31:23 +0200

    Atualizar o page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     return (
         <main className='flex-1'>
         <div className='flex justify-between align-center p-3 h-14 border-b border-border'>
-            <div className='flex gap-1.5'>
+            <div className='flex justify-center align-center gap-1.5'>
               <NavMenu />
               <h2 className='text-md'>Home</h2>
             </div>
