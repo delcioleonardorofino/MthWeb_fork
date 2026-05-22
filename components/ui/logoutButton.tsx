d19c60ef761b96046ee04e437f7fc6aca6609136
delcioleonardorofino <delcioleonard@gmail.com>
2026-05-22 20:32:47 +0200

    Atualizar o logoutButton.tsx

--- components/ui/logoutButton.tsx
+++ components/ui/logoutButton.tsx
@@ -13,7 +13,7 @@ async function logOut() {
 export default function Logoutbutton () {
     return (
         <form action={logOut}>
-            <button className="bg-background p-1.5 border border-border rounded-md text-muted-foreground self-center hover:text-foreground hover:bg-accent transition-colors">
+            <button className="bg-accent p-1.5 rounded-md text-muted-foreground self-center hover:text-foreground hover:bg-muted transition-colors">
                     <LogOut size={20} fill='none' stroke='currentColor' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' aria-hidden='true' />
             </button>
         </form>
