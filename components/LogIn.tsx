ae5847761db34cef393c274bb838ae1c6ce5c3d1
unknown <delcioleonard>
2026-05-22 09:55:40 -0700

    Added google povider for auth

--- components/LogIn.tsx
+++ components/LogIn.tsx
@@ -2,12 +2,16 @@ import { signIn } from '@/auth';
 import Link from 'next/link';
 import { ArrowLeft } from 'lucide-react';
 import { FaGithub as GitHubIcon } from 'react-icons/fa';
+import { FcGoogle as GoogleIcon } from 'react-icons/fc';
 import Menu from '@/components/Menu';
 // import {Button} from '@/components/ui/button';
 
-async function signTo(){
+async function signTo(formData: FormData){
     'use server'
-    await signIn("github", {
+
+    const prov = formData.get('provider') as string;
+    
+    await signIn(prov, {
         redirectTo: "/home"
     })
 }
@@ -41,11 +45,23 @@ export default function LogInComponent (){
                 <form action={signTo} className='space-y-4'>
                     <button
                     type='submit'
+                    name='provider'
+                    value='github'
                     className='w-full flex items-center justify-center gap-2 cursor-pointer py-3
                     border border-border rounded-md text-sm font-medium text-foreground hover:bg-accent/50 transition-colors duration-200'
                     >
                         <GitHubIcon className='mr-2 h-4 w-4 ' />
                         Entrar com GitHub
+                    </button>
+                    <button
+                    type='submit'
+                    name='provider'
+                    value='google'
+                    className='w-full flex items-center justify-center gap-2 cursor-pointer py-3
+                    border border-border rounded-md text-sm font-medium text-foreground hover:bg-accent/50 transition-colors duration-200'
+                    >
+                        <GoogleIcon className='mr-2 h-4 w-4 ' />
+                        Entrar com Google
                     </button>
                 </form>
             </div>
---
+++ components/ui/logoutButton.tsx
@@ -0,0 +1,21 @@
+import { signOut } from '@/auth';
+import { LogOut } from 'lucide-react';
+
+
+async function logOut() {
+    "use server"
+
+    await signOut({
+        redirectTo: '/'
+    })
+} 
+
+export default function Logoutbutton () {
+    return (
+        <form action={logOut}>
+            <button className="bg-background p-1.5 rounded-md text-muted-foreground self-center hover:text-foreground hover:bg-accent transition-colors">
+                <LogOut height={8} width={8} />
+            </button>
+        </form>
+    )
+}
