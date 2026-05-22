2c78ccb509174261f8b1cace545962ed75c7bc11
delcioleonardorofino <delcioleonard@gmail.com>
2026-05-22 20:15:03 +0200

    Atualizar o page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,7 +5,10 @@ export default function HomePage() {
         <main className='flex-1'>
         <div className='flex justify-between align-center p-3 h-14 border-b border-border'>
             <h2 className='text-md'>Home</h2>
-            <LogoutButton />          
+            <div className='flex gap-2'>
+            <LogoutButton />
+           
+            </div>          
         </div>
         <div className='flex justify-center align-center h-screen mt-18'>
             <h1 className='text-2xl font-bold tracking-tight text-foreground sm:text-2xl'>
