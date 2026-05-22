83abe9fc233c418fbf6c6a06eebc318bdc31ce39
unknown <delcioleonard>
2026-05-22 01:35:22 -0700

    Added header to login and removed from layut

--- app/entrar/page.tsx
+++ app/entrar/page.tsx
@@ -1,11 +1,28 @@
-import { signIn } from '@/auth'
+import { signIn } from '@/auth';
+import Link from 'next/link';
+import { ArrowLeft } from 'lucide-react'
+import Menu from '@/components/Menu'
+
 
 export default function LoginPage (){
     return (
+        <>
+        <div className='flex justify-between align-center p-4 h-14 border-b border-border'>
+            <Link 
+            href='/' 
+            className='inline-flex items-center justify-center px-3 py-3 text-sm text-foreground rounded-md hover:bg-accent transition-colors'
+            >
+             <ArrowLeft size={18} fill='none' stroke='currentColor' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' aria-hidden='true' /> 
+             Voltar
+             </Link>
+
+            <Menu />
+        </div>
         <main className='flex justify-center align-center pt-24'>
             <div className='flex flex-col max-w-6xl p-4 lg:p-6'>
-                Login Here!
+                <div></div>
             </div>
         </main>
+        </>
     )
 }
