e4c93f781a51952b779ec43343ac879cb0aa7bab
delcioleonardorofino <delcioleonard@gmail.com>
2026-05-24 12:42:17 +0200

    Atualizar o MobileNavMenu.tsx

--- components/MobileNavMenu.tsx
+++ components/MobileNavMenu.tsx
@@ -60,7 +60,7 @@ export default function MobileNavMenu() {
       className={`
         fixed bottom-0 left-0 z-50
         border-t border-border
-        pt-1 px-4 pb-2
+        pt-1 p-4      pb-[calc(1rem+env(safe-area-inset-bottom))]
         flex justify-between items-center
         w-full gap-3
         md:hidden
