9d7203ad11b3f2311a0338858aaa1e997c1ae768
unknown <delcioleonard>
2026-05-29 11:26:50 -0700

    created feed posts and added some other pages

--- app/artigos/page.tsx
+++ app/artigos/page.tsx
@@ -42,7 +42,7 @@ export default async function Users(){
                         height={32} />
                         }
                     </div>
-            )}
+            ))}
         </div>
     )
 }
